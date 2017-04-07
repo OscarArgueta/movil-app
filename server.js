5ce74252aafb5c96abbaf01afa9011d9cb3e5985
Oscar A. Argueta H <oscar.argueta@atel.com.gt>
2017-04-07 17:16:55 -0600

    Primer intento de uso de Angular.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
     res.send('Test Ok!');
 })
 
+app.get('/index.html', function(req, res){
+  res.sendFile(__dirname + "/" + "index.html");  
+})
+
 var server = app.listen(3000, function() {
     var host = server.address().address
     var port = server.address().port
