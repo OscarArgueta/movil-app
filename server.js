3a3a23ba444318cc0c60a19bccb87048c486a961
Oscar A. Argueta H <oscar.andres.argueta@gmail.com>
2017-04-12 06:18:46 -0600

    Prueba con Angular
    Se agrego en los tag html las directrices para poder utilizar correctamente angular
    Se corrigio el tag type (mal escrito) de la libreria angular.js

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/index.html', function(req, res){
   res.sendFile(__dirname + "/" + "index.html");  
 })
 
+
+app.get('/test.html', function(req, res){
+  res.sendFile(__dirname + "/" + "test.html");  
+})
+
 var server = app.listen(3000, function() {
     var host = server.address().address
     var port = server.address().port
